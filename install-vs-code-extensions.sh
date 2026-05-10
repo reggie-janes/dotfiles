972ce18692e21e900e8e0d96c599d8e0d263b68e
Reggie Janes <reggie_janes@proton.me>
2026-05-10 16:59:44 +0300

    Add debug prints.

--- install-vs-code-extensions.sh
+++ install-vs-code-extensions.sh
@@ -8,7 +8,9 @@ set -exuo pipefail
 
 echo "install-vs-code-extensions.sh"
 
-code --install-extension anthropic.claude-code --force || true
-code --install-extension vscode-icons-team.vscode-icons --force || true
+fail=0
+code --install-extension anthropic.claude-code --force || fail=1
+code --install-extension vscode-icons-team.vscode-icons --force || fail=1
 
-echo "install-vs-code-extensions.sh done"
+echo "install-vs-code-extensions.sh done (fail=$fail)"
+exit $fail
